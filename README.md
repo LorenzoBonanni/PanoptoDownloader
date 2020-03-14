309882b1b4dd6b4a21580c12fde147924a0d2e70
LorenzoBonanni <lorenzo.bonanni@studenti.univr.it>
2020-03-14 18:48:06 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # PanoptoDownloader
 A Python Script to download lessons from panopto
+## How to Use:
+### Italian: 
+#### Requisiti:
+1) [Python 3.x](https://www.html.it/pag/15609/installare-python/)
+2) [pip](https://www.html.it/pag/346968/pip-il-package-manager-per-python/)
+3) [Chrome](https://www.google.com/chrome/) 
+4) [ChromeDriver](https://chromedriver.chromium.org/downloads)<br/>
+   la versione win32 funziona sia su dispositivi a 32 che a 64bit
+#### Avviare il Programma
+1) Rinominare il Chrome driver in ```chromedriver``` e inserirlo nella cartella config
+2) Aprire la riga di comando navigare fino alla cartella del progetto
+3) Installare le Dipendenze con il Comando: <br /> ```pip install -r requirements.txt```
+4) Inserire le credenziali GIA nel file [config.yaml](./config/config.yaml) seguendo questa struttura: <br/>
+    ```yaml
+    username: id123abc
+    password: password1234
+    ```
+5) Inserire i link ai video da scaricare e il nome della materia nel file [videos.yaml](./config/videos.yaml) con la seguente struttura:<br />
+    ```yaml
+    Architettura degli elaboratori:
+        - htttp:\\example-link1.com
+        - htttp:\\example-link2.com
+    subject2:
+        - htttp:\\example-link1.com
+        - htttp:\\example-link2.com
+    ```
+6) Avviare il programma con il comando: ```python main.py``` 
+
+### English: 
+#### Requirements:
+1) [Python 3.x](https://realpython.com/installing-python/)
+2) [pip](https://www.makeuseof.com/tag/install-pip-for-python/)
+3) [Chrome](https://www.google.com/chrome/) 
+4) [ChromeDriver](https://chromedriver.chromium.org/downloads)<br/>
+   the win32 version works on both windows 32 and 64bit
+#### Run the Program
+1) Rename Chrome Driver into ```chromedriver``` and then insert it into the config folder
+2) Open CommandLine and go to the project directory 
+3) Install dependencies with the command: <br /> ```pip install -r requirements.txt```
+4) Insert GIA credentials into the file [config.yaml](./config/config.yaml) with the following structure: <br/> 
+    ```yaml 
+    username: id123abc 
+    password: password1234
+    ```
+5) Insert video links and subject name into the file [videos.yaml](./config/videos.yaml) with the following structure:<br />
+    ```yaml
+    Architettura degli elaboratori:
+        - htttp:\\example-link1.com
+        - htttp:\\example-link2.com
+    subject2:
+        - htttp:\\example-link1.com
+        - htttp:\\example-link2.com
+    ```
+6) Start the program with the command: ```python main.py``` 
---
+++ main.py
@@ -0,0 +1,81 @@
+import time
+
+import yaml
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import pyautogui
+import urllib.request
+
+options = Options()
+options.add_extension("config/elicpjhcidhpjomhibiffojpinpmmpil.crx")
+driver = webdriver.Chrome(executable_path="config/chromedriver", options=options)
+config_file_name = "config/config.yaml"
+with open(config_file_name) as f:
+    config_file = yaml.load(stream=f, Loader=yaml.BaseLoader)
+    username = config_file["username"]
+    password = config_file["password"]
+
+
+def get_videos() -> dict:
+    """
+    open video config files, retrieves data and returns it
+    :return:
+    """
+
+    with open("config/videos.yaml", 'r') as v:
+        return yaml.load(stream=v, Loader=yaml.BaseLoader)
+
+
+def login():
+    """
+    goes to panopto main page and makes login
+    :return:
+    """
+
+    driver.get("https://univr.cloud.panopto.eu/Panopto/Pages/Home.aspx")
+    button = driver.find_elements_by_id("loginButton")[0]
+    button.click()
+    button = driver.find_elements_by_class_name("input-wrapper")[1]
+    button.click()
+    username_field = driver.find_element_by_id("IDToken1")
+    username_field.send_keys(username)
+    password_field = driver.find_element_by_id("IDToken2")
+    password_field.send_keys(password)
+    send_button = driver.find_element_by_class_name("btn")
+    send_button.click()
+
+
+def download_video(subject: str, url: str):
+    """
+    download video lessons
+    :param subject: the subject of the video that is being downloaded
+    :param url: the url from which the program will download the file
+    :return:
+    """
+
+    driver.get(url)
+    # wait till the page is completely load
+    time.sleep(5)
+    # save lesson name
+    lesson = driver.find_elements_by_id("deliveryTitle")[0].text
+    # locate the extension on the screen searching for its icon
+    v = pyautogui.locateOnScreen("./config/icon.png", confidence=.5)
+    # click extension button
+    pyautogui.click(x=v[0], y=v[1], clicks=1, interval=0.0, button="left")
+    # click download button
+    pyautogui.click(x=v[0], y=v[1] * 2, clicks=1, interval=0.0, button="left")
+    source_tag = driver.find_element_by_tag_name("source")
+    urllib.request.urlretrieve(source_tag.get_attribute("src"), f"output/{subject} {lesson}.mp4")
+
+
+def main():
+    login()
+    videos = get_videos()
+    for subject in videos:
+        for url in videos[subject]:
+            download_video(subject, url)
+    driver.quit()
+
+
+if __name__ == '__main__':
+    main()
